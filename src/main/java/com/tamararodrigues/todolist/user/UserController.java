d5e1dd6cfdb0f8e46a41698a988e8d5d719c824a
TamaraRodrigues <tamaramayer2014@gmail.com>
2026-09-04 13:59:48 +0100

    Initial commit

---
+++ src/main/java/com/tamararodrigues/todolist/user/UserController.java
@@ -0,0 +1,30 @@
+package com.tamararodrigues.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLOutput;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public UserModel create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            System.out.println("Usuario ja existe!");
+            return null;
+        }
+        var userCreated = this.userRepository.save(userModel);
+       return userCreated;
+    }
+
+}
